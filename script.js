fdb82f7a9076cbdf98e1f4735d3dad385d3aedbe
Marija Gorbunova <mariarapirovna@gmail.com>
2025-02-17 12:10:21 +0200

    finish

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 document.getElementById("changeText").addEventListener("click", function() {
-    alert("Nupp klõpsatud! Rakendage oma funktsioone.");
+showInfo()
 });
+function showInfo() {
+    var h4Elements = document.getElementsByTagName("h4");
+    var body = document.body;
+    inf = document.getElementById("info");
+    inf.style.opacity = 1;
+    inf.style.transition = "opacity 1s";
+    body.style.backgroundColor = "black";
+    body.style.transition = "background-color 1s";
+    for (var i = 0; i < h4Elements.length; i++) {
+        h4Elements[i].style.color = "white";
+    }
+}
